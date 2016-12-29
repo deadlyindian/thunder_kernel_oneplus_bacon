83da28e4c1077d3f434d81260f45021752559d5c
alucard24 <dmbaoh2@gmail.com>
2016-12-29 11:26:33 -0200

    cpufreq: import and setup darkness governor
    Yet simple it could be...
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -441,8 +441,10 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
-
 
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
